5bee62ceb6b4d9b6288a0518e5ac485060d9423f
Jehon <1582670+jehon@users.noreply.github.com>
2022-07-11 21:21:51 +0200

    feat: first script

---
+++ config.sh
@@ -0,0 +1,21 @@
+!/usr/bin/env bash
+
+# pcloud shared-root app
+# configured here: https://docs.pcloud.com/my_apps/
+export JH_PASS_PCLOUD_RO_CLIENT_ID="U2FsdGVkX19hW7/a8hxb6X+/xWEaRAylMx4Y78laL8Y="
+export JH_PASS_PCLOUD_RO_CLIENT_SECRET="U2FsdGVkX18BM5skF0UwiytcxPoyqfHQoCEETOLsrJm+DtPvQ1u2Lq2tESxnc+1b"
+export JH_PASS_PCLOUD_RW_CLIENT_ID="U2FsdGVkX1+wHolIYEiPXUmcp2L0zVeKj5EQ3ODhEbs="
+export JH_PASS_PCLOUD_RW_CLIENT_SECRET="U2FsdGVkX18SgIaCVlVe8Ixg+ILkWQiy9FsEnpALY8jzuIWI4szwnR97hxhcMsDE"
+
+export JH_PASS_CAMERA_USER="U2FsdGVkX18DVNatc+bToj+5E0LWY3NE4KeL91FrCGs="
+export JH_PASS_CAMERA_PASS="U2FsdGVkX1+mtUqmulHFiMpJNUq7PBjxy/N+HPs4P1Q="
+
+export JH_DOCKER_HUB_TOKEN="U2FsdGVkX1/bWiFY4wrAYDyioZ01cnaTfnDzYaVtTy0N79H+bcnMSAg/0N2sB3vawMuNq8XSt30cqsbR9hibKw=="
+
+export JH_DEFAULT_PWD="U2FsdGVkX1+kT31LidX6KgzOU0Q8awukdkCtyjmxOg8="
+
+# Not imported:
+
+# JH_NAS_USER
+# JH_NAS_PASS
+# JH_HOSTS_KIOSK_IP
