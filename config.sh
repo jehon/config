f670ff2e2f909c45bc53f0b96926a8fa7449bd4f
Jean Honlet <root@e9e0e6fdea4a>
2022-07-12 07:27:13 +0000

    fix: add secret mark

--- config.sh
+++ config.sh
@@ -1,18 +1,20 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
+
+export JH_SECRET_MARK="🔒"
 
 # pcloud shared-root app
 # configured here: https://docs.pcloud.com/my_apps/
-export JH_PASS_PCLOUD_RO_CLIENT_ID="U2FsdGVkX19hW7/a8hxb6X+/xWEaRAylMx4Y78laL8Y="
-export JH_PASS_PCLOUD_RO_CLIENT_SECRET="U2FsdGVkX18BM5skF0UwiytcxPoyqfHQoCEETOLsrJm+DtPvQ1u2Lq2tESxnc+1b"
-export JH_PASS_PCLOUD_RW_CLIENT_ID="U2FsdGVkX1+wHolIYEiPXUmcp2L0zVeKj5EQ3ODhEbs="
-export JH_PASS_PCLOUD_RW_CLIENT_SECRET="U2FsdGVkX18SgIaCVlVe8Ixg+ILkWQiy9FsEnpALY8jzuIWI4szwnR97hxhcMsDE"
+export JH_PASS_PCLOUD_RO_CLIENT_ID="🔒U2FsdGVkX19hW7/a8hxb6X+/xWEaRAylMx4Y78laL8Y="
+export JH_PASS_PCLOUD_RO_CLIENT_SECRET="🔒U2FsdGVkX18BM5skF0UwiytcxPoyqfHQoCEETOLsrJm+DtPvQ1u2Lq2tESxnc+1b"
+export JH_PASS_PCLOUD_RW_CLIENT_ID="🔒U2FsdGVkX1+wHolIYEiPXUmcp2L0zVeKj5EQ3ODhEbs="
+export JH_PASS_PCLOUD_RW_CLIENT_SECRET="🔒U2FsdGVkX18SgIaCVlVe8Ixg+ILkWQiy9FsEnpALY8jzuIWI4szwnR97hxhcMsDE"
 
-export JH_PASS_CAMERA_USER="U2FsdGVkX18DVNatc+bToj+5E0LWY3NE4KeL91FrCGs="
-export JH_PASS_CAMERA_PASS="U2FsdGVkX1+mtUqmulHFiMpJNUq7PBjxy/N+HPs4P1Q="
+export JH_PASS_CAMERA_USER="🔒U2FsdGVkX18DVNatc+bToj+5E0LWY3NE4KeL91FrCGs="
+export JH_PASS_CAMERA_PASS="🔒U2FsdGVkX1+mtUqmulHFiMpJNUq7PBjxy/N+HPs4P1Q="
 
-export JH_DOCKER_HUB_TOKEN="U2FsdGVkX1/bWiFY4wrAYDyioZ01cnaTfnDzYaVtTy0N79H+bcnMSAg/0N2sB3vawMuNq8XSt30cqsbR9hibKw=="
+export JH_DOCKER_HUB_TOKEN="🔒U2FsdGVkX1/bWiFY4wrAYDyioZ01cnaTfnDzYaVtTy0N79H+bcnMSAg/0N2sB3vawMuNq8XSt30cqsbR9hibKw=="
 
-export JH_DEFAULT_PWD="U2FsdGVkX1+kT31LidX6KgzOU0Q8awukdkCtyjmxOg8="
+export JH_DEFAULT_PWD="🔒U2FsdGVkX1+kT31LidX6KgzOU0Q8awukdkCtyjmxOg8="
 
 # Not imported:
 
